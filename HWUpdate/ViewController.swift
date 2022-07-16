f40589e25ffa5a66fc6911cdf5e450904c6b5b8f
r0zari0 <maksymstovolos2002@gmail.com>
2022-07-16 17:21:39 -0400

    Initial Commit

---
+++ HWUpdate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWUpdate
+//
+//  Created by Max Stovolos on 7/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
